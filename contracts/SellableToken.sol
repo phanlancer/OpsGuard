92fc5d86183d371f24c6d29ebabd3028561d33dc
Phan Trung Sinh <phanlancerduk@hotmail.com>
2020-04-13 08:46:06 +0700

    define constructor

--- contracts/SellableToken.sol
+++ contracts/SellableToken.sol
@@ -1,22 +1,20 @@
 pragma solidity >=0.4.19 <0.6.5;
 
-
 import "./OpsToken.sol";
 import "./Multivest.sol";
 
 
 contract SellableToken is Multivest {
-
     OpsToken public token;
 
     uint256 public constant DECIMALS = 18;
 
-    uint256 public minPurchase = 1000000;//10usd * 10 ^ 5
+    uint256 public minPurchase = 1000000; //10usd * 10 ^ 5
 
-    uint256 public softCap = 300000000000;//usd * 10 ^ 5
-    uint256 public hardCap = 1500000000000;//usd * 10 ^ 5
+    uint256 public softCap = 300000000000; //usd * 10 ^ 5
+    uint256 public hardCap = 1500000000000; //usd * 10 ^ 5
 
-    uint256 public compensationAmount = 5100000000;//usd * 10 ^ 5
+    uint256 public compensationAmount = 5100000000; //usd * 10 ^ 5
     uint256 public compensatedAmount;
 
     uint256 public startTime;
@@ -38,9 +36,9 @@ contract SellableToken is Multivest {
 
     uint256 public etherPriceInUSD; //$753.25  75325000
 
-    mapping (address => uint256) public etherBalances;
+    mapping(address => uint256) public etherBalances;
 
-    mapping (address => bool) public whitelist;
+    mapping(address => bool) public whitelist;
 
     Tier[] public tiers;
 
@@ -55,22 +53,25 @@ contract SellableToken is Multivest {
 
     event Refund(address _holder, uint256 _ethers, uint256 _tokens);
 
-    function SellableToken(
+    constructor(
         address _token,
         address _etherHolder,
         address _compensationAddress,
         uint256 _etherPriceInUSD,
         uint256 _maxTokenSupply
-    )
-        public Multivest()
-    {
+    ) public Multivest() {
         require(_token != address(0));
         token = OpsToken(_token);
 
-        require(_etherHolder != address(0) && _compensationAddress != address(0));
+        require(
+            _etherHolder != address(0) && _compensationAddress != address(0)
+        );
         etherHolder = _etherHolder;
         compensationAddress = _compensationAddress;
-        require((_maxTokenSupply == uint256(0)) || (_maxTokenSupply <= token.maxSupply()));
+        require(
+            (_maxTokenSupply == uint256(0)) ||
+                (_maxTokenSupply <= token.maxSupply())
+        );
 
         etherPriceInUSD = _etherPriceInUSD;
         maxTokenSupply = _maxTokenSupply;
@@ -79,7 +80,9 @@ contract SellableToken is Multivest {
     }
 
     function() public payable {
-        require(true == whitelist[msg.sender] && buy(msg.sender, msg.value) == true);
+        require(
+            true == whitelist[msg.sender] && buy(msg.sender, msg.value) == true
+        );
     }
 
     function setTokenContract(address _token) public onlyOwner {
@@ -105,18 +108,28 @@ contract SellableToken is Multivest {
         }
     }
 
-    function setCompensationAddress(address _compensationAddress) public onlyOwner {
+    function setCompensationAddress(address _compensationAddress)
+        public
+        onlyOwner
+    {
         if (_compensationAddress != address(0)) {
             compensationAddress = _compensationAddress;
         }
     }
 
-    function updateWhitelist(address _address, bool isWhitelisted) public onlyOwner {
+    function updateWhitelist(address _address, bool isWhitelisted)
+        public
+        onlyOwner
+    {
         whitelist[_address] = isWhitelisted;
         WhitelistSet(_address, isWhitelisted);
     }
 
-    function mint(address _address, uint256 _tokenAmount) public onlyOwner returns (uint256) {
+    function mint(address _address, uint256 _tokenAmount)
+        public
+        onlyOwner
+        returns (uint256)
+    {
         return mintInternal(_address, _tokenAmount);
     }
 
@@ -124,7 +137,10 @@ contract SellableToken is Multivest {
         setEtherInUSDInternal(_price);
     }
 
-    function setEtherInUSD(string _price) public onlyAllowedMultivests(msg.sender) {
+    function setEtherInUSD(string _price)
+        public
+        onlyAllowedMultivests(msg.sender)
+    {
         setEtherInUSDInternal(_price);
     }
 
@@ -136,9 +152,9 @@ contract SellableToken is Multivest {
         uint256 dot = bytePrice.length.sub(uint256(6));
 
         // check if dot is in 6 position  from  the last
-        require(0x2e == uint(bytePrice[dot]));
+        require(0x2e == uint256(bytePrice[dot]));
 
-        uint256 newPrice = uint256(10 ** 23).div(parseInt(_price, 5));
+        uint256 newPrice = uint256(10**23).div(parseInt(_price, 5));
 
         require(newPrice > 0);
 
@@ -147,12 +163,17 @@ contract SellableToken is Multivest {
         priceUpdateAt = block.timestamp;
     }
 
-    function mintInternal(address _address, uint256 _tokenAmount) internal returns (uint256) {
+    function mintInternal(address _address, uint256 _tokenAmount)
+        internal
+        returns (uint256)
+    {
         uint256 mintedAmount = token.mint(_address, _tokenAmount);
 
         require(mintedAmount == _tokenAmount);
 
-        mintedAmount = mintedAmount.add(token.mint(compensationAddress, _tokenAmount.mul(5).div(1000)));
+        mintedAmount = mintedAmount.add(
+            token.mint(compensationAddress, _tokenAmount.mul(5).div(1000))
+        );
 
         soldTokens = soldTokens.add(_tokenAmount);
         if (maxTokenSupply > 0) {
@@ -165,7 +186,9 @@ contract SellableToken is Multivest {
     function transferEthersInternal() internal {
         if (collectedUSD >= softCap) {
             if (compensatedAmount < compensationAmount) {
-                uint256 amount = uint256(1 ether).mul(compensationAmount.sub(compensatedAmount)).div(etherPriceInUSD);
+                uint256 amount = uint256(1 ether)
+                    .mul(compensationAmount.sub(compensatedAmount))
+                    .div(etherPriceInUSD);
                 compensationAddress.transfer(amount);
                 compensatedAmount = compensationAmount;
             }
@@ -174,22 +197,21 @@ contract SellableToken is Multivest {
         }
     }
 
-    function parseInt(string _a, uint _b) internal pure returns (uint) {
+    function parseInt(string _a, uint256 _b) internal pure returns (uint256) {
         bytes memory bresult = bytes(_a);
-        uint mintt = 0;
+        uint256 mintt = 0;
         bool decimals = false;
-        for (uint i=0; i<bresult.length; i++){
-            if ((bresult[i] >= 48)&&(bresult[i] <= 57)){
-                if (decimals){
+        for (uint256 i = 0; i < bresult.length; i++) {
+            if ((bresult[i] >= 48) && (bresult[i] <= 57)) {
+                if (decimals) {
                     if (_b == 0) break;
                     else _b--;
                 }
                 mintt *= 10;
-                mintt += uint(bresult[i]) - 48;
+                mintt += uint256(bresult[i]) - 48;
             } else if (bresult[i] == 46) decimals = true;
         }
         if (_b > 0) mintt *= 10**_b;
         return mintt;
     }
-
 }
