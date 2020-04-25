d1ce3d448003c27ac65670d473100ed6e727bcde
Phan Trung Sinh <phanlancerduk@hotmail.com>
2020-04-25 08:08:52 +0700

    update TestOpsToken

--- contracts/test/TestOpsToken.sol
+++ contracts/test/TestOpsToken.sol
@@ -1,16 +1,15 @@
-pragma solidity ^0.4.18;
+pragma solidity >=0.4.19 <0.6.5;
 
 import "../OpsToken.sol";
 
+
 contract TestOpsToken is OpsToken {
-    function TestOpsToken(
-        address _bountyAddress,
-        bool _locked
-    ) public OpsToken(_bountyAddress, _locked)
-    { }
+    function TestOpsToken(address _bountyAddress, bool _locked)
+        public
+        OpsToken(_bountyAddress, _locked)
+    {}
 
     function testSetFreezing(bool _isFrozen) public {
         transferFrozen = _isFrozen;
     }
-
 }
